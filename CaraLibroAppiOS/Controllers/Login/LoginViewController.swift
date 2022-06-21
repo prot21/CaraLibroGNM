4b7e2e02319db61c1e7e4db88361cdc1aaabfad4
Gera <j21nmc@gmail.com>
2022-06-21 01:51:59 -0500

    vid 2 min 23

---
+++ CaraLibroAppiOS/Controllers/Login/LoginViewController.swift
@@ -0,0 +1,113 @@
+//
+//  LoginViewController.swift
+//  CaraLibroAppiOS
+//
+//  Created by Gera Munoz on 20/06/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.clipsToBounds = true
+        return scrollView
+    }()
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "logo")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private let emailField: UITextField = {
+        let field = UITextField()
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.returnKeyType = .continue
+        field.layer.cornerRadius = 12
+        field.layer.borderWidth = 1
+        field.layer.borderColor  = UIColor.lightGray.cgColor
+        field.placeholder = "Correo electrónico"
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 0))
+        field.leftViewMode = .always
+        field.backgroundColor = .white
+        return field
+    }()
+    
+    private let passwordField: UITextField = {
+        let field = UITextField()
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.returnKeyType = .done
+        field.layer.cornerRadius = 12
+        field.layer.borderWidth = 1
+        field.layer.borderColor  = UIColor.lightGray.cgColor
+        field.placeholder = "Contraseña"
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 0))
+        field.leftViewMode = .always
+        field.backgroundColor = .white
+        field.isSecureTextEntry = true
+        return field
+    }()
+    
+    private let loginButton: UIButton = {
+        let button  = UIButton()
+        button.setTitle("Iniciar sesión", for: .normal)
+        button.backgroundColor = .link
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 12
+        button.layer.masksToBounds = true
+        button.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
+        return button
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Iniciar Sesión"
+        view.backgroundColor = .white
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Registrarse",
+                                                                    style: .done,
+                                                                    target: self,
+                                                                    action: #selector(didTapRegister))
+        //Anadir subviews
+        view.addSubview(scrollView)
+        scrollView.addSubview(imageView)
+        scrollView.addSubview(emailField)
+        scrollView.addSubview(passwordField)
+        scrollView.addSubview(loginButton)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        scrollView.frame = view.bounds
+        let size = scrollView.width/3
+        imageView.frame = CGRect(x: (scrollView.width-size)/2,
+                                 y: 20,
+                                 width: size,
+                                 height: size)
+        emailField.frame = CGRect(x: 30,
+                                  y: imageView.bottom+10,
+                                  width: scrollView.width-60,
+                                  height: 52)
+        passwordField.frame = CGRect(x: 30,
+                                     y: emailField.bottom+10,
+                                     width: scrollView.width-60,
+                                     height: 52)
+        loginButton.frame = CGRect(x: 30,
+                                     y: passwordField.bottom+10,
+                                     width: scrollView.width-60,
+                                     height: 52)
+                                    
+    }
+    
+    @objc private func didTapRegister() {
+        let vc = RegisterViewController()
+        vc.title = "Crear cuenta"
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+}
---
+++ CaraLibroAppiOS/Controllers/Login/RegisterViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RegisterViewController.swift
+//  CaraLibroAppiOS
+//
+//  Created by Gera Munoz on 20/06/22.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .link
+    }
+    
+
+
+
+}
