dcd57cf3ac22171fd09fd9c4bbd47a2fdc0de746
Gera <j21nmc@gmail.com>
2022-07-10 17:05:45 -0500

    vid parte 8 min 27

---
+++ CaraLibroAppiOS/Controllers/ChatViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ChatViewController.swift
+//  CaraLibroAppiOS
+//
+//  Created by Gera Munoz on 10/07/22.
+//
+
+import UIKit
+import MessageKit
+
+struct Message: MessageType{
+    var sender: SenderType
+    var messageId: String
+    var sentDate: Date
+    var kind: MessageKind
+}
+
+struct Sender: SenderType{
+    var photoURL: String
+    var senderId: String
+    var displayName: String
+}
+
+class ChatViewController: MessagesViewController {
+    
+    private var messages = [Message]()
+    
+    private let selfSender = Sender(photoURL: "",
+                                    senderId: "1",
+                                    displayName: "Gera Munoz")
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        messages.append(Message(sender: selfSender,
+                                messageId: "1",
+                                sentDate: Date(),
+                                kind: .text("Mensaje hola mundo")))
+        
+        messages.append(Message(sender: selfSender,
+                                messageId: "1",
+                                sentDate: Date(),
+                                kind: .text("Mensaje hola mundo hola mundo hola mundo hola mundo hola mundo hola mundo")))
+        
+        view.backgroundColor = .red
+        
+        messagesCollectionView.messagesDataSource = self
+        messagesCollectionView.messagesLayoutDelegate = self
+        messagesCollectionView.messagesDisplayDelegate = self
+    }
+
+}
+
+extension ChatViewController: MessagesDataSource, MessagesLayoutDelegate, MessagesDisplayDelegate {
+    func currentSender() -> SenderType {
+        return selfSender
+    }
+    
+    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType {
+        return messages[indexPath.section]
+    }
+    
+    func numberOfSections(in messagesCollectionView: MessagesCollectionView) -> Int {
+        return messages.count
+    }
+    
+}
+
--- CaraLibroAppiOS/Controllers/ConversationsViewController.swift
+++ CaraLibroAppiOS/Controllers/ConversationsViewController.swift
@@ -7,13 +7,41 @@
 
 import UIKit
 import FirebaseAuth
+import JGProgressHUD
 
 class ConversationsViewController: UIViewController {
+    
+    private let spinner = JGProgressHUD(style: .dark)
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.isHidden = true
+        table.register(UITableViewCell.self,
+                        forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    private let noConversationsLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Sin conversaciones"
+        label.textAlignment = .center
+        label.textColor = .gray
+        label.font = .systemFont(ofSize: 21, weight: .medium)
+        label.isHidden = true
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.addSubview(tableView)
+        view.addSubview(noConversationsLabel)
+        setupTableView()
+        fetchConversations()
+    }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
     }
     
     override func viewDidAppear(_ animated : Bool) {
@@ -30,5 +58,37 @@ class ConversationsViewController: UIViewController {
         }
     }
     
+    private func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    private func fetchConversations() {
+        tableView.isHidden = false
+    }
+    
 }
+
+extension ConversationsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Maria Luisa"
+        cell.accessoryType = .disclosureIndicator
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let vc = ChatViewController()
+        vc.title = "Maria Luisa"
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
+    }
+}
+
 
